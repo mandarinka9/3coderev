4bb96616886eb7bd88ee1df0bd1e7db36efc2fd3
mandarinka9 <145179517+mandarinka9@users.noreply.github.com>
2025-04-25 02:07:49 +0500

    Update and rename graph.h to morse.h

---
+++ 1/morse.h
@@ -0,0 +1,20 @@
+#ifndef MORSE_H
+#define MORSE_H
+
+#include <string>
+#include <unordered_map>
+#include <locale>
+#include <cctype>
+
+using namespace std;
+
+// Таблица соответствия русских букв и пробела азбуке Морзе
+extern const unordered_map<char, string> MORSE_CODE;
+
+// Проверка, что символ поддерживается таблицей Морзе
+bool validateInput(char c);  // Возвращает false при ошибке
+
+// Преобразование строки в азбуку Морзе
+bool convertToMorse(const string& input, string& result);
+
+#endif
