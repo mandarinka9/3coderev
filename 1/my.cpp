b966cf0c5131e882dfcedecdad3767a327f8b68e
mandarinka9 <145179517+mandarinka9@users.noreply.github.com>
2025-04-25 02:06:37 +0500

    Update my.cpp

--- 1/my.cpp
+++ 1/my.cpp
@@ -2,6 +2,7 @@
 Напишите программу-телеграф, которая принимает от пользователя сообщение и выводит
 его на экран в виде последовательности точек и тире.
 */
+
 #include "morse.h"
 #include <iostream>
 
@@ -18,20 +19,23 @@ const unordered_map<char, string> MORSE_CODE = {
     {' ', " "}
 };
 
-void validateInput(char c) {
+bool validateInput(char c) {
     c = toupper((unsigned char)c);
     if (MORSE_CODE.find(c) == MORSE_CODE.end()) {
         cerr << "Ошибка: Неподдерживаемый символ: " << c << endl;
-        throw invalid_argument("Неподдерживаемый символ");
+        return false;
     }
+    return true;
 }
 
-string convertToMorse(const string& input) {
-    string result;
+bool convertToMorse(const string& input, string& result) {
+    result.clear();
     for (char c : input) {
         c = toupper((unsigned char)c);
-        validateInput(c);
+        if (!validateInput(c)) {
+            return false;
+        }
         result += MORSE_CODE.at(c) + " ";
     }
-    return result;
+    return true;
 }
