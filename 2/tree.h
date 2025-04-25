188506385eba704bce292be69fae2e00ccbe8771
mandarinka9 <145179517+mandarinka9@users.noreply.github.com>
2025-04-25 14:13:39 +0500

    Update tree.h

--- 2/tree.h
+++ 2/tree.h
@@ -6,21 +6,63 @@
 
 using namespace std;
 
-// Структура узла дерева выражений
+/**
+ * @brief Структура узла дерева выражений
+ * 
+ * Содержит целочисленное значение (операнд или код операции),
+ * а также указатели на левое и правое поддеревья.
+ */
 struct TreeNode {
-    int value;     
-    TreeNode* left;  
-    TreeNode* right; 
+    int value;     ///< Значение узла (операнд 0-9 или код операции: -1..-4)
+    TreeNode* left;  ///< Указатель на левое поддерево
+    TreeNode* right; ///< Указатель на правое поддерево
 
+    /**
+     * @brief Конструктор узла дерева
+     * @param val Значение узла
+     */
     explicit TreeNode(int val) : value(val), left(nullptr), right(nullptr) {}
 };
 
-// Прототипы функций
+/**
+ * @brief Читает токены из файла
+ * @param filename Имя файла для чтения
+ * @return Вектор строк-токенов
+ */
 vector<string> ReadTokensFromFile(const string& filename);
+
+/**
+ * @brief Строит дерево из префиксной записи
+ * @param tokens Вектор токенов
+ * @param index Текущий индекс в векторе токенов (передается по ссылке)
+ * @return Указатель на корень построенного дерева
+ */
 TreeNode* BuildTreeFromPrefix(const vector<string>& tokens, int& index);
+
+/**
+ * @brief Вычисляет значение поддерева
+ * @param node Корень поддерева
+ * @return Результат вычисления поддерева
+ */
 int EvaluateSubtree(TreeNode* node);
+
+/**
+ * @brief Преобразует дерево, заменяя операции сложения/вычитания их результатами
+ * @param root Корень дерева для преобразования
+ * @return Указатель на корень преобразованного дерева
+ */
 TreeNode* TransformTree(TreeNode* root);
+
+/**
+ * @brief Выводит дерево в инфиксной форме
+ * @param root Корень дерева для печати
+ */
 void PrintTree(const TreeNode* root);
+
+/**
+ * @brief Удаляет дерево и освобождает память
+ * @param root Корень дерева для удаления
+ */
 void DeleteTree(TreeNode* root);
 
-#endif 
+#endif
